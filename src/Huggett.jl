a20637efd175849703c5c93a2d2dccc2ed37baaf
FGerding <97884526+FGerding@users.noreply.github.com>
2022-01-23 23:50:31 +0100

    Update Huggett.jl

--- src/Huggett.jl
+++ src/Huggett.jl
@@ -58,7 +58,7 @@ export interest, β, σ, borrow, itmx_m, tol_v, tol_m, itmx_v, vmin, eps_clear, 
 
 
 """ 
-function AssetGrid(borrow)
+	AssetGrid(borrow)
 
 Creates a grid of equally spaced points ranging from the minimum level of assets, i.e. the borrowing constraint level `borrow` (which has to be specified by the user). 
 The maximum number of assets `a_max = 24` and the number of assets `na=500` have been specified for the sake of the replication exercise.
@@ -83,16 +83,16 @@ end
 
 
 """ 
-function crra(c,σ)
+	crra(c,σ)
 
 Computes the utility associated to a level of consumption c, when the utility function is a CRRA utility with constant relative risk aversion parameter equal to `σ`. Remember that a CRRA utility function takes the form ``\frac{c^{1-\sigma}}{1-\sigma}``.
 
 # Inputs:
 
 - `c::Float64`: the level of consumption for which we want to compute the associated utility. Notice that `c>=0` must hold.
--`σ::Float64`: relative risk aversion parameter of the CRRA utility function. 
+- `σ::Float64`: relative risk aversion parameter of the CRRA utility function. 
 
-#Outputs:
+# Outputs:
 
 - A scalar of type Float64 indicating the utility level.
 """ 
@@ -106,16 +106,16 @@ function crra(c,σ)
 end
 
 """ 
-function initguess(r, borrow)
+	initguess(r, borrow)
 
 Computes an initial guess for the Value Function to start the Value Function Iteration (VFI) procedure used to solve the household problem of the Huggett economy. It is not necessary to run this function in order to solve the household problem, since the Contraction Mapping Theorem guarantees the convergence of the Value Function Iteration procedure, starting from any Value Function Guess. However, running this function makes the code faster. 
 
 # Inputs:
 
 - `r::interest`: the interest rate used to use to solve the household’s problem.
--`borrow::Float64` : the borrowing constraint level to be specified by the user.
+- `borrow::Float64` : the borrowing constraint level to be specified by the user.
 
-#Outputs:
+# Outputs:
 
 - `ns*na` vector of type Vector{Float64} to be used as a starting guess of the VFI procedure, where `na` is the number of assets and `ns` is the number of states for the income process.
 """ 
@@ -135,15 +135,15 @@ end
 
 
 """ 
-function next_guess(V,pol, borrow)
+	next_guess(V,pol, borrow)
 
 Implements the Howard’s improvement algorithm, which is used to update the value function after each step of the value function iteration procedure.  The basic idea of Howard’s Improvement Step is that instead of finding a maximizer for each iteration of the VFI procedure, the same maximizer should be used repeatedly for a specified  number of iterations (e.g. 70). The resulting Value Function will be the starting point of the following step of the VFI procedure. 
 
 # Inputs:
 
--`V::Vector{Float64}` : Vector of dimension `ns*na` which represents the Value function obtained at the end of any step of the VFI procedure.
--`pol::Vector{Int64}` : Policy Function is a column vector of length `ns x na` coming from a given iteration of the VFI procedure.
--`borrow::Float64` : Borrowing constraint level to be specified by the user.
+- `V::Vector{Float64}` : Vector of dimension `ns*na` which represents the Value function obtained at the end of any step of the VFI procedure.
+- `pol::Vector{Int64}` : Policy Function is a column vector of length `ns x na` coming from a given iteration of the VFI procedure.
+- `borrow::Float64` : Borrowing constraint level to be specified by the user.
 
 # Outputs:
 
@@ -169,15 +169,15 @@ end
 
 
 """ 
-function household(v,r, borrow)
+	household(v,r, borrow)
 
 Implements the VFI iteration procedure in order to solve the household problem of the Huggett economy for a given interest rate `r` and given a level of borrowing constraint `borrow`. The Value function obtained from  [`initguess`](@ref) is used as an input, serving as the initial guess in the VFI procedure. Furthermore, the function calls [`next_guess`](@ref) at the end of each step of the VFI procedure: in this way Howard’s improvement algorithm is used in order to obtain the next guess of the VFI procedure. 
 
 # Inputs: 
 
--`v::Vector{Float64}` : Initial guess for Value Function.
--`r::interest` : Interest rate to be used for solving the household problem.
--`borrow::Float64`: Borrowing constraint level to be specified by the user.
+- `v::Vector{Float64}` : Initial guess for Value Function.
+- `r::interest` : Interest rate to be used for solving the household problem.
+- `borrow::Float64`: Borrowing constraint level to be specified by the user.
 
 # Output: 
 
@@ -212,13 +212,13 @@ findnearest(A,x) = argmin(abs.(A .- x))
 
 		
 """ 
-function lambda(pol_new)
+	lambda(pol_new)
 
 Taking as input the policy function obtained solving Huggett’s household problem, this function computes the implied stationary distribution of assets in the economy. 
 
 # Inputs:
 
--`pol_new::Vector{Int64}` : the Policy Function is a column vector of length `ns*na` coming from a given iteration of the VFI procedure.
+- `pol_new::Vector{Int64}` : the Policy Function is a column vector of length `ns*na` coming from a given iteration of the VFI procedure.
 
 # Outputs:
 
@@ -239,17 +239,17 @@ function lambda(pol_new)
 end
 		
 """ 
-function eD(pol_new,lambda, borrow)
+	eD(pol_new,lambda, borrow)
 
 This function retrieves the amount of excess demand in the economy given the Policy Function, the asset’s stationary distribution and the level of borrowing constraints. Stated in an equivalent way, given any level of interest rate ‘r’ this function takes the policy function coming from [`household`](@ref), and the induced stationary distribution of assets coming from [`lambda`](@ref) in order to compute the excess demand of the considered Huggett economy (with level of borrowing constraint ‘borrow’).
 
-#Inputs:
+# Inputs:
 
--`pol_new::Vector{Int64}` : the Policy Function is a column vector of length ’ns*na’ coming from the solution of the household problem when the given interest rate is `r`.
--`lambda::Vector{Int64}`: the stationary distribution of assets in the Huggett economy. 
--`borrow::Float64` : the borrowing constraint level imposed on the Huggett economy.
+- `pol_new::Vector{Int64}` : the Policy Function is a column vector of length ’ns*na’ coming from the solution of the household problem when the given interest rate is `r`.
+- `lambda::Vector{Int64}`: the stationary distribution of assets in the Huggett economy. 
+- `borrow::Float64` : the borrowing constraint level imposed on the Huggett economy.
 
-#Outputs:
+# Outputs:
 
 - A scalar of type Float64 indicating the excess demand in the considered Huggett economy. Notice that if excess demand = 0 then we the interest rate which delivers ‘pol_new’ as a policy function is the general equilibrium interest rate!
 """
